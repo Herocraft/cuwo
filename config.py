33ca87962423a6ffd68f93d443b3d11559431bf2
TehNinth <staffofawe@hotmail.co.uk>
2013-07-19 22:56:12 +0100

    added name stuff

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 # Global server name
-server_name = 'cuwo server'
+server_name = 'Herocube'
 
 # Max number of players on the server at once
 max_players = 4
@@ -10,7 +10,7 @@ max_players = 4
 seed = 26879
 
 # Time speed modifier, 1.0 is default
-time_modifier = 1.0
+time_modifier = 10.0
 
 # List of scripts to run on the server on startup.
 # Consider turning on 'pvp', i.e. player versus player
@@ -29,10 +29,10 @@ welcome = ["Welcome to %(server_name)s!",
            "(server powered by cuwo)"]
 
 # IRC script variables (enable by adding 'irc' to script list)
-irc_nickname = 'cuwobot'
+irc_nickname = 'hcbot'
 irc_server = 'irc.esper.net'
 irc_port = 6667
-irc_channel = '#cuwo.bots'
+irc_channel = '#herocubechat'
 irc_password = None
 irc_commandprefix = '.'
 irc_chatprefix = '#'
