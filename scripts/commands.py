90a26a5dfde67763028995ffad42f1aa2fb3d3c6
TehNinth <staffofawe@hotmail.co.uk>
2013-07-20 11:03:18 +0100

    https://github.com/matpow2/cuwo/commit/5747686105687b2d30d592e2a76209563ce9ca5b

--- scripts/commands.py
+++ scripts/commands.py
@@ -97,29 +97,41 @@ def pm(script, name, *args):
     return 'PM sent to player.name'
     bot.me('%s to %s (PM): %s' % (script.connection.name, player.name, message))
 
-@irc    
-@command
-@admin
-def kill(script, name):
-    player = get_player(script.server, name)
+def damage_player(script, player, damage=0, stun_duration=0):
     packet = HitPacket()
     packet.entity_id = player.entity_id
     packet.target_id = player.entity_id
     packet.hit_type = HIT_NORMAL
-    packet.damage = player.entity_data.hp + 1000.0
+    packet.damage = damage
     packet.critical = 1
-    packet.stun_duration = 0
+    packet.stun_duration = stun_duration
     packet.something8 = 0
     packet.pos = player.position
     packet.hit_dir = Vector3()
     packet.skill_hit = 0
     packet.show_light = 0
     script.server.update_packet.player_hits.append(packet)
+
+@irc
+@command
+@admin
+def kill(script, name):
+    player = get_player(script.server, name)
+    damage_player(script, player, damange=player.entity_data.hp + 100.0)
     message = '%s was killed' % player.name
     print message
-    bot.me(message)
     script.server.send_chat(message)
 
+@irc
+@command
+@admin
+def stun(script, name, stun_duration=500):
+    player = get_player(script.server, name)
+    damage_player(script, player, stun_duration=stun_duration)
+    message = '%s was stunned' % player.name
+    print message
+    script.server.send_chat(message)
+    
 @irc
 @command
 @admin
